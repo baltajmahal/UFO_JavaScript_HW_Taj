5068fae24a12b64e2169f3b75907e7f05edb8795
baltajmahal <43283518+baltajmahal@users.noreply.github.com>
2019-02-11 18:25:22 -0500

    Add files via upload

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,30 +1,56 @@
 // from data.js
 var tableData = data;
+console.log(tableData)
 
-// YOUR CODE HERE!
 
-//--1--collect the value from the input field
-//--2--filter the data for the data inputed
-//--3--use the foreach toappend the data of each cell
-var button = d3.select("#filter-btn");
+//Getting a reference to the Table boday on the page with the element name.
+var tbody = d3.select("tbody");
 
-button.on("click", function(){
+// // YOUR CODE HERE!
 
-    d3.event.preventDefault();
-
-    var query = d3.select("#datetime").property("value");
-    console.log(query);
-    var filtered_results = tableData.filter(d => d.datetime === query)
-    console.log(filtered_results);
-
-    var tbody = d3.select("tbody");
-
-    filtered_results.forEach(function(entry) {
-        var row = tbody.append("tr")
-        Object.entries(entry).forEach(function([key,value]) {
-          row.append("td").text(value);
-        });
+tableData.forEach((UFOsight) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOsight).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
     });
-    
+  });
 
+  ////an Event listener to filter the Data based on the value entered inside the #input-field
+  // Event handlers are just normal functions that can do anything you want
+var filterbtn = d3.select("#filter-btn");
+
+filterbtn.on("click", function() {
+  //to prevent the whole page to refresh
+  d3.event.preventDefault();
+
+  //We need to clear the table body before we can display the filtered data 
+  var vtbody = d3.select('tbody');
+  vtbody.text(" ");
+  console.log(vtbody);
+
+  //select the input element from the index html file
+  var inputElement = d3.select("#datetime");
+
+  //Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  //validate the system is capturing the entered value.
+  console.log("value is used", inputValue);
+
+  //filtering
+  var filteredData = tableData.filter(sight => sight.datetime === inputValue);
+
+  //cosole the filtered data
+  console.log("the filtered data", filteredData);
+  
+
+  //append the filtered data into the html table body
+  filteredData.forEach(function(UFOfilter) {
+    var row = tbody.append("tr");
+    Object.entries(UFOfilter).forEach(function ([key, value]) {
+      console.log(key, value);
+      var cell1 = tbody.append("td");
+      cell1.text(value);
+    });
+  });  
 });
